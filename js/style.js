63fd0e41ee3df5ae2a57705169f17e6be51b4acb
Polina <rudichrudichrudich@mail.ru>
2022-02-23 17:49:44 +0300

    aa

--- js/style.js
+++ js/style.js
@@ -2,36 +2,49 @@
 'use strict';
 const menuLinks = document.querySelectorAll('.menu-link[data-goto]');
 const menuHeader = document.querySelector('.header-menu');
+const arrowButtonsBefore = document.getElementsByClassName('circle-animated');
+const arrowButtonAfter = document.querySelector('.start-mining');
+var textAnimated = document.createElement('div');
+var circleAnimated = document.createElement('div');
+var arrowAnimated = document.createElement('div');
+var element  = document.querySelector(".circle-animated");
+var bigArrow = document.querySelector(".arrow-circle");
 
-if(menuLinks.length>0){
-    menuLinks.forEach(menuLink=>{
-        menuLink.addEventListener("click",onMenuLinkClick);
-    });
-    function onMenuLinkClick(e){
-        const menuLink = e.target;
-        if(menuLink.dataset.goto&&document.querySelector(menuLink.dataset.goto)){
-            const gotoBlock = document.querySelector(menuLink.dataset.goto);
-            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
-
-            if(burger.classList.contains('_active')){
-                burger.classList.remove('_active');
-                document.body.classList.remove('_lock');
-                menuHeader.classList.remove('_active');
-            };
-            window.scrollTo({
-                top:gotoBlockValue,
-                behavior:"smooth"
-            });
-            e.preventDefault();
-            
-        }
-    }
-}
 const burger = document.querySelector('.burger');
-if(burger){
-    burger.addEventListener("click", function(e){
+if(burger) {
+    burger.addEventListener("click", function(e) {
         burger.classList.toggle('_active');
         document.body.classList.toggle('_lock');
         menuHeader.classList.toggle('_active');
     });
 }
+if(arrowButtonsBefore) {
+
+    for (let arrowButtonBefore of arrowButtonsBefore) {
+        arrowButtonBefore.addEventListener("mouseenter", function(e) {
+            
+            console.log(e);
+            bigArrow.classList.toggle('_visibility-collapsed');
+            element.appendChild(circleAnimated);
+            element.appendChild(textAnimated);
+            circleAnimated.appendChild(arrowAnimated);
+            textAnimated.classList.toggle("start-mining-animated-text");
+            textAnimated.innerHTML = "Start mining now";
+            circleAnimated.classList.toggle('try-arrow');
+            arrowAnimated.classList.toggle('arrow');
+            arrowButtonBefore.classList.toggle('_passive');
+            arrowButtonAfter.classList.toggle('_active');
+        });
+
+        arrowButtonBefore.addEventListener('mouseleave', function(e){
+            arrowButtonBefore.classList.toggle('_passive');
+            arrowButtonAfter.classList.toggle('_active');
+            circleAnimated.classList.toggle('try-arrow');
+            arrowAnimated.classList.toggle('arrow');
+            bigArrow.classList.toggle('_visibility-collapsed');
+            textAnimated.classList.toggle("start-mining-animated-text");
+        })
+    };
+        
+}
+
