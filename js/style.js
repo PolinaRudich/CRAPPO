71951249ee6a264202c82ff23a716d7114cfa3fa
Polina <rudichrudichrudich@mail.ru>
2022-02-27 18:15:10 +0300

    bb

--- js/style.js
+++ js/style.js
@@ -18,33 +18,5 @@ if(burger) {
         menuHeader.classList.toggle('_active');
     });
 }
-if(arrowButtonsBefore) {
 
-    for (let arrowButtonBefore of arrowButtonsBefore) {
-        arrowButtonBefore.addEventListener("mouseenter", function(e) {
-            
-            console.log(e);
-            bigArrow.classList.toggle('_visibility-collapsed');
-            element.appendChild(circleAnimated);
-            element.appendChild(textAnimated);
-            circleAnimated.appendChild(arrowAnimated);
-            textAnimated.classList.toggle("start-mining-animated-text");
-            textAnimated.innerHTML = "Start mining now";
-            circleAnimated.classList.toggle('try-arrow');
-            arrowAnimated.classList.toggle('arrow');
-            arrowButtonBefore.classList.toggle('_passive');
-            arrowButtonAfter.classList.toggle('_active');
-        });
-
-        arrowButtonBefore.addEventListener('mouseleave', function(e){
-            arrowButtonBefore.classList.toggle('_passive');
-            arrowButtonAfter.classList.toggle('_active');
-            circleAnimated.classList.toggle('try-arrow');
-            arrowAnimated.classList.toggle('arrow');
-            bigArrow.classList.toggle('_visibility-collapsed');
-            textAnimated.classList.toggle("start-mining-animated-text");
-        })
-    };
-        
-}
 
