d9a5d69f26d73dbd27dbfaf36c103865fa3a23ab
Polina <rudichrudichrudich@mail.ru>
2022-02-27 19:03:25 +0300

    make rates work with API

--- js/style.js
+++ js/style.js
@@ -9,6 +9,8 @@ var circleAnimated = document.createElement('div');
 var arrowAnimated = document.createElement('div');
 var element  = document.querySelector(".circle-animated");
 var bigArrow = document.querySelector(".arrow-circle");
+var ratesGrid = document.querySelector(".image-analitics-grid");
+
 
 const burger = document.querySelector('.burger');
 if(burger) {
@@ -19,4 +21,33 @@ if(burger) {
     });
 }
 
+function createRateItem(rateData) {
+    return `
+    <img src="${rateData.logoSource}" class="bitcoin-card" alt="">
+    <div class="bitcoin-text">
+        <div class="bitcoin-abbr">${rateData.currencyShortName}</div>
+        <div class="bitcoin-title">${rateData.currencyName}</div>
+    </div>
+    <div class="bitcoin-in-dollars">$${rateData.price}</div>
+    <div class="percent-of-bitcoin">${rateData.percentageChange}</div>
+    <div class="volume-of-bitcoin">$${rateData.volume}</div>
+    `;
+}
 
+fetch('https://my-json-server.typicode.com/PolinaRudich/CRAPPO/rates')
+    .then((response) => response.json())
+    .then((json) => createRatesFromJson(json));
+
+function createRatesFromJson(json) {
+    let ratesHtml = '';
+    json.forEach(element => {
+        if(element.percentageChange > 0)
+        {
+            element.percentageChange = "+" + element.percentageChange;
+        }
+        element.percentageChange = element.percentageChange + "%";
+        let rateHtml = createRateItem(element);
+        ratesHtml = ratesHtml + rateHtml;
+    });
+    ratesGrid.innerHTML = ratesHtml;
+}
