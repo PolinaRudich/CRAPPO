fa478458c2eb4443c642649652a25c5e11eab891
Polina <rudichrudichrudich@mail.ru>
2022-03-02 15:41:06 +0300

    add setInterval

--- js/style.js
+++ js/style.js
@@ -7,23 +7,22 @@ const arrowButtonAfter = document.querySelector('.start-mining');
 var textAnimated = document.createElement('div');
 var circleAnimated = document.createElement('div');
 var arrowAnimated = document.createElement('div');
-var element  = document.querySelector(".circle-animated");
+var element = document.querySelector(".circle-animated");
 var bigArrow = document.querySelector(".arrow-circle");
 var ratesGrid = document.querySelector(".image-analitics-grid");
 var array = new Array();
 
 
 const burger = document.querySelector('.burger');
-if(burger) {
-    burger.addEventListener("click", function(e) {
+if (burger) {
+    burger.addEventListener("click", function (e) {
         burger.classList.toggle('_active');
         document.body.classList.toggle('_lock');
         menuHeader.classList.toggle('_active');
     });
 }
 
-function createRateItem(rateData) {
-
+function createRateItem(rateData, id) {
     return `
     <img src="${rateData.logoSource}" class="bitcoin-card" alt="">
     <div class="bitcoin-text">
@@ -31,7 +30,7 @@ function createRateItem(rateData) {
         <div class="bitcoin-title">${rateData.currencyName}</div>
     </div>
     <div class="bitcoin-in-dollars">$${rateData.price}</div>
-    <div class="percent-of-bitcoin"></div>
+    <div id="${id}" class="percent-of-bitcoin"></div>
     <div class="volume-of-bitcoin">$${rateData.volume}</div>
     `;
 }
@@ -40,42 +39,49 @@ function createRateItem(rateData) {
 
 fetch('https://my-json-server.typicode.com/PolinaRudich/CRAPPO/rates')
     .then((response) => response.json())
-    .then((json) =>{ createRatesFromJson(json);
-                      createPercentage(json);});
+    .then((json) => { createRatesFromJson(json); });
 
 function createRatesFromJson(json) {
     let ratesHtml = '';
     let rateLiteCoinHtml = '';
-    json.forEach(element => {
-
-        array = element.percentageChange;
-        if(element.percentageChange > 0)
-        {
-            element.percentageChange = "+" + element.percentageChange;
-        }
-        element.percentageChange = element.percentageChange + "%";
-        let rateHtml = createRateItem(element);
+    let percentageIdTemplate = "percent";
+    json.forEach((element, index) => {
+        var currentId = percentageIdTemplate + index;
+        let rateHtml = createRateItem(element, currentId);
         ratesHtml = ratesHtml + rateHtml;
+
+
     });
     ratesGrid.innerHTML = ratesHtml;
+    json.forEach((element, index) => {
+        var currentId = percentageIdTemplate + index;
+        createPercentage(element, currentId);
+    });
 }
 
-function createPercentage(json){
-    var percentOfBitcoin = document.querySelector('.percent-of-bitcoin');
-    json.forEach(element=>{
-        setInterval(function(){
-            array = element.percentageChange;
-            let s = array.replace(/[\s,%]/g, ' ').trim();
-            var arr = s.split(' ');
-            arr = arr.filter(Boolean);
-            for(let i= 0; i<arr.length; ){
-                i++;
-                percentOfBitcoin.innerHTML = arr[i];
-                
-                
-            }
-        },2000)
-    })
+function createPercentage(json, id) {
+    var percentOfBitcoin = document.querySelector('#' + id);
+    var priceOfBitcoin = document.querySelector('#'+id);
+    var i = 0;
+
+    setInterval(function () {
+        priceOfBitcoin.innerHTML = json.price[i];
+        percentOfBitcoin.innerHTML = getPercentageString(json.percentageChange[i]);
+        if (i + 1 == json.percentageChange.length) {
+            i = 0;
+        }
+        else {
+            i++;
+        }
+
+    }, 2000);
 
 }
 
+
+function getPercentageString(percentage) {
+    if (percentage > 0) {
+        percentage = "+" + percentage;
+    }
+    return percentage + "%";
+}
